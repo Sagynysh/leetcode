70ca291442599d1cfffdd39b45c74f8476fdb266
Sagynysh Kenel <sagynyshkenel@MacBook-Pro-1.local>
2020-05-15 09:56:54 +0600

    leetcode may challenge +1

--- src/main/java/randomSample.java
+++ src/main/java/randomSample.java
@@ -12,9 +12,24 @@ public class randomSample {
 
     }
 
-1 1 1
-1 1 0
-1 0 1
+    public String removeKdigits(String num, int k) {
+        StringBuffer str = new StringBuffer(num);
+        for(int i =0; i<k;i++){
+            int j = 0;
+            while(i<str.length()-1 && str.charAt(j)<str.charAt(j+1)){
+                j++;
+            }
+            str.delete(j,j+1);
+        }
+        while(str.charAt(0) == '0')
+            str.delete(0,1);
+
+        if(str.length() == 0){
+            return "0";
+        }
+        return str.toString();
+
+    }
 
     public static int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         if(newColor != image[sr][sc]){
